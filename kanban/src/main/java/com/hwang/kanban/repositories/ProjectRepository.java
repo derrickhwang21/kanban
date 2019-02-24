e5ba05fb44eee25aa0cae718687dc11edd7d67bc
dhwang21 <hwang.derrick@gmail.com>
2019-02-24 15:34:20 -0800

    added repo and entity model

---
+++ kanban/src/main/java/com/hwang/kanban/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.hwang.kanban.repositories;
+
+
+import com.hwang.kanban.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
