0c23c4062615993a822b48cd42151fd6f533ffef
dhwang21 <hwang.derrick@gmail.com>
2019-02-24 21:11:06 -0800

    added in model validation handler, controller for get and delete

--- kanban/src/main/java/com/hwang/kanban/services/ProjectService.java
+++ kanban/src/main/java/com/hwang/kanban/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.hwang.kanban.services;
 
 import com.hwang.kanban.domain.Project;
+import com.hwang.kanban.exceptions.ProjectIdException;
 import com.hwang.kanban.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,35 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '" +project.getProjectIdentifier().toUpperCase()+ "' already exists");
+        }
+    }
 
-        return projectRepository.save(project);
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot Project with ID '" +projectId + "' does not exist");
+        }
+        projectRepository.delete(project);
     }
 }
