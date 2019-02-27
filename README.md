02b7b95c41c842979078ac3716fdb4130af7f1a2
dhwang21 <hwang.derrick@gmail.com>
2019-02-26 17:44:36 -0800

    migrated to mysql database, added in backlog and projecttask not found exception handlers

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kanban_java-react
 A project management application built with Spring for backend services and React + Redux as frontend framework
+
+## Running the Server
+
+#### Booting up MySQL database locally
+Ensure your MySQL database is connected to database `kanban` in MySQL shell
--- kanban/src/main/java/com/hwang/kanban/services/ProjectTaskService.java
+++ kanban/src/main/java/com/hwang/kanban/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.hwang.kanban.services;
 
 import com.hwang.kanban.domain.Backlog;
+import com.hwang.kanban.domain.Project;
 import com.hwang.kanban.domain.ProjectTask;
+import com.hwang.kanban.exceptions.ProjectNotFoundException;
 import com.hwang.kanban.repositories.BacklogRepository;
+import com.hwang.kanban.repositories.ProjectRepository;
 import com.hwang.kanban.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,30 +19,53 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
+        try{
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
 
-        Integer BacklogSequence = backlog.getPTSequence();
-        BacklogSequence++;
-        
-        backlog.setPTSequence(BacklogSequence);
+            Integer BacklogSequence = backlog.getPTSequence();
+            BacklogSequence++;
 
-        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            backlog.setPTSequence(BacklogSequence);
+
+            projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
 
-        if(projectTask.getStatus() == "" || projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
+            if(projectTask.getStatus() == "" || projectTask.getStatus()==null){
+                projectTask.setStatus("TO_DO");
+            }
+
+            if(projectTask.getPriority() == null){
+                projectTask.setPriority(3);
+            }
+
+
+            return projectTaskRepository.save(projectTask);
+
+        }catch(Exception e){
+            throw new ProjectNotFoundException("Project not found");
         }
 
-        if(projectTask.getPriority() == null){
-            projectTask.setPriority(3);
+
+    }
+
+    public Iterable<ProjectTask> findBacklogById(String id){
+        Project project = projectRepository.findByProjectIdentifier(id);
+        if(project == null){
+            throw new ProjectNotFoundException("Project with ID: '" +id+ "' does not exist");
         }
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
 
 
-        return projectTaskRepository.save(projectTask);
-
+        return projectTaskRepository.findByProjectSequence(pt_id);
     }
 }
